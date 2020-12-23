1e1828f610f0f5f23f649af9f3d3cb8d2df97bcc
pasha13d <mahabub13d1995@gmail.com>
2020-12-23 18:24:07 +0600

    jwt token with core api

---
+++ JWTToken/Context/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using JWTToken.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTToken.Context
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Users> users { get; set; }
+        public DbSet<UserInfo> userInfo { get; set; }
+    }
+}
